26279398c556b4736647495c34ee8060dff8ceae
JanjeVuk <janjevuk@outlook.be>
2024-01-07 17:32:05 +0100

    Register new ConnectionManager in Player Events
    Introduced the ConnectionManager class to manage player events, specifically connection-related ones. The ConnectionManager is now registered in the Player Events section, improving the flexibility and customization options for join and quit messages sourced from a configuration file.

--- src/main/java/net/etum/etumatrium/Listeners/Events.java
+++ src/main/java/net/etum/etumatrium/Listeners/Events.java
@@ -1,12 +1,25 @@
 package net.etum.etumatrium.Listeners;
 
 import net.etum.etumatrium.Main;
+import net.etum.etumatrium.Player.Events.ConnectionManager;
+import org.bukkit.plugin.PluginManager;
 
 public class Events {
 
 
+
     public Events(Main main) {
 
+        PluginManager pm = main.getServer().getPluginManager();
+
+
+        eventsPlayer(main, pm);
+
+    }
+
+    private void eventsPlayer(Main main, PluginManager pm){
+
+        pm.registerEvents(new ConnectionManager(), main);
 
     }
 }
