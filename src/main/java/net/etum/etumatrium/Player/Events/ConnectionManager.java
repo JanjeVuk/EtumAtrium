dbe6d082996612cd5afb94f5cd86d1abbb3a67f5
JanjeVuk <janjevuk@outlook.be>
2024-01-20 20:39:18 +0100

    Add detailed comments to Connection Manager methods
    This update adds comprehensive and clear comments for the methods within the Connection Manager class. The comments detail the purpose of each method, along with the parameters they take, effectively improving the understandability of the code.

--- src/main/java/net/etum/etumatrium/Player/Events/ConnectionManager.java
+++ src/main/java/net/etum/etumatrium/Player/Events/ConnectionManager.java
@@ -17,21 +17,41 @@ public class ConnectionManager implements Listener {
     private static final String QUIT_MSG_KEY = "Module.ConnectionManager.message.quit";
     private static final String WELCOME_MSG = getWelcomeMsg();
 
+    /**
+     * Returns the welcome message for players.
+     *
+     * @return The welcome message.
+     */
     private static String getWelcomeMsg() {
         FileConfiguration config = Main.getInstance().getConfig();
         return String.join(System.lineSeparator(), config.getStringList("Module.ConnectionManager.message.welcome"));
     }
 
+    /**
+     * Applies a player event message to the given event.
+     *
+     * @param event The player join event.
+     */
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
         applyPlayerEventMessage(event, JOIN_MSG_KEY);
     }
 
+    /**
+     * Applies a player event message to the given event.
+     *
+     * @param event The player quit event.
+     */
     @EventHandler
     public void onPlayerQuit(PlayerQuitEvent event) {
         applyPlayerEventMessage(event, QUIT_MSG_KEY);
     }
 
+    /**
+     * Applies a player event message to the given event.
+     *
+     * @param event       The player event.
+     * @param messageKey  The key*/
     private void applyPlayerEventMessage(PlayerEvent event, String messageKey) {
         FileConfiguration config = Main.getInstance().getConfig();
         if (!config.getBoolean("Module.ConnectionManager.enable")) {
