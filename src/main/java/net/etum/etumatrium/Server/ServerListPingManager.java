b48c3ccab45a14d3ddce6e7f22019ec043ddb218
janjevuk <janjevuk@outlook.be>
2024-01-21 17:24:01 +0100

    Implement MOTD functionality to ServerListPingManager
    Added functionality to the ServerListPingManager that uses server's configuration to fetch, format, and display Message of The Day (MOTD) upon server ping. The 'MODULE.MOTD.enable' config option controls whether to display this message or not, providing flexibility in server management.

--- src/main/java/net/etum/etumatrium/Server/ServerListPingManager.java
+++ src/main/java/net/etum/etumatrium/Server/ServerListPingManager.java
@@ -1,20 +1,30 @@
 package net.etum.etumatrium.Server;
 
 import net.etum.etumatrium.Main;
+import net.kyori.adventure.text.Component;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.server.ServerListPingEvent;
 
+import java.util.List;
+
 public class ServerListPingManager implements Listener {
 
     @EventHandler
     public void onServerListPing(ServerListPingEvent event) {
-
         FileConfiguration config = Main.getInstance().getConfig();
+        if (!config.getBoolean("MODULE.MOTD.enable")) return;
 
-
+        List<String> messageList = config.getStringList("MODULE.MOTD.message");
+        event.motd(
+                Component.text(
+                        messageList.get(0).replace('&', '§') + "\n" + messageList.get(1).replace('&', '§')
+                )
+        );
     }
+
+
 
 
 
